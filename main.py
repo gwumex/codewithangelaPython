b375fb7fd1f9bbb758cc1cebacf4fad57cafd504
Olawunmi Salami <42099116+gwumex@users.noreply.github.com>
2023-07-17 18:38:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#day 1
+Print("Hello world")
+
+#string concatenation
+print("Hello" + " " + "world")
+
+#input function
+print("hello" + input("what is your name? "))
+
+#variables
+
+name = "jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+#input function 
+
+name = input("What is your name?")
+length = len(name)
+
+print(length)
+
+#type check
+name = input("Whats your name? ")
+type = type(input)
+print(type)
